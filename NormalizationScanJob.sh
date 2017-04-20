cfb394c658d1b9155f496789bbe8d425496840ff
Ryan Reece <ryan.reece@cern.ch>
2017-04-20 18:29:21 -0500

    NormalizationScan

---
+++ NormalizationScanJob.sh
@@ -0,0 +1,17 @@
+#PBS -V
+printenv
+mkdir -p ScanLogs
+output=ScanLogs/$PBS_ARRAYID.log
+
+echo $output >> $output
+echo Running on $HOSTNAME >> $output
+echo Array Number: $PBS_ARRAYID >> $output
+echo Queue: $PBS_QUEUE >> $output
+
+cd ~/LCD/DLKit
+source setup.sh
+
+python -m CaloDNN.ClassificationExperiment -C NormalizationScanConfig.py -s $PBS_ARRAYID &>> $output
+
+
+
